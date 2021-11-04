1d15e0bcca01b2221b22737aa6656e82b75d003b
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-04 13:34:13 +0300

    add field

---
+++ src/by/overone/homeworks/homework21/Field.java
@@ -0,0 +1,21 @@
+package by.overone.homeworks.homework21;
+
+
+public class Field {
+    String [][] field = {{" "," "," "},
+                         {" "," "," "},
+                         {" "," "," "},};
+
+
+    public StringBuffer printField(){
+        StringBuffer str = new StringBuffer();
+        str.append(" " + field[0][0]+ " | " + field[0][1] + " | " + field[0][2]);
+        str.append("\n---+---+---\n");
+        str.append(" " + field[1][0]+ " | " + field[1][1] + " | " + field[1][2]);
+        str.append("\n---+---+---\n");
+        str.append(" " + field[2][0]+ " | " + field[2][1] + " | " + field[2][2]);
+        return str;
+    }
+
+}
+
---
+++ src/by/overone/homeworks/homework21/Main.java
@@ -0,0 +1,11 @@
+package by.overone.homeworks.homework21;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    Field field = new Field();
+        System.out.println(field.printField());
+    }
+}
