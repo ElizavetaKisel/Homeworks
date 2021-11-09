43fe1e0ff5d7dbd13563108e5c9dea054032453b
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-09 21:45:51 +0300

    create method add

--- src/by/overone/homework23/Main.java
+++ src/by/overone/homework23/Main.java
@@ -1,4 +1,20 @@
 package by.overone.homework23;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Num <Integer> intNum = new Num(1000);
+        Num <Byte> byteNum = new Num((byte)12);
+        Num <Short> shortNum = new Num((short)32767);
+        Num <Long> longNum = new Num(805400193L);
+        Num <Double> doubleNum = new Num(3.3);
+        Num <Float> floatNum = new Num(1.12322342f);
+
+        var result = longNum.add(floatNum);
+        if (result == null){
+            System.out.println("Error. Out of bounds type size.");
+        }else {
+            System.out.println(result);
+        }
+    }
 }
--- src/by/overone/homework23/Num.java
+++ src/by/overone/homework23/Num.java
@@ -2,21 +2,73 @@ package by.overone.homework23;
 
 public class Num <N extends Number>{
     private N num;
+    public Num(){
 
-    public Num add(Num<?> x){
-        return new Num();
     }
 
-    public Num subtract(Num<?> x){
-        return new Num();
+    public Num(N num){
+       this.num = num;
     }
 
-    public Num multiply(Num<?> x){
-        return new Num();
+
+    public N add(Num<?> x){
+        Double result = num.doubleValue() + x.getNum().doubleValue();
+
+        Class<? extends Number> type = num.getClass();
+        switch (type.getSimpleName()){
+            case "Integer":
+                if (result > Integer.MAX_VALUE | result < Integer.MIN_VALUE) {
+                    return null;
+                }else {
+                    return (N) (Integer) result.intValue();
+                }
+            case "Byte":
+                if (result > Byte.MAX_VALUE | result < Byte.MIN_VALUE) {
+                    return null;
+                }else {
+                    return (N) (Byte) result.byteValue();
+                }
+            case "Short":
+                if (result > Short.MAX_VALUE | result < Short.MIN_VALUE) {
+                    return null;
+                }else {
+                    return (N) (Short) result.shortValue();
+                }
+            case "Long":
+                if (result > Long.MAX_VALUE | result < Long.MIN_VALUE) {
+                    return null;
+                }else {
+                    return (N) (Long) result.longValue();
+                }
+            case "Float":
+                if (result == Float.POSITIVE_INFINITY | result == Float.NEGATIVE_INFINITY |
+                        Float.compare(-0.0f, result.floatValue()) == 0 | Float.compare(+0.0f, result.floatValue()) == 0) {
+                    return null;
+                }else {
+                    return (N) (Float) result.floatValue();
+                }
+            case "Double":
+                if (result == Double.POSITIVE_INFINITY | result == Double.NEGATIVE_INFINITY |
+                        Double.compare(-0.0f, result) == 0 | Double.compare(+0.0f, result) == 0) {
+                    return null;
+                }else {
+                    return (N) result;
+                }
+        }
+
+        return null;
     }
 
-    public Num divide(Num<?> x){
-        return new Num();
+    public N subtract(Num<?> x){
+        return null;
+    }
+
+    public N multiply(Num<?> x){
+        return null;
+    }
+
+    public N divide(Num<?> x){
+        return null;
     }
 
     public N getNum() {
@@ -27,3 +79,4 @@ public class Num <N extends Number>{
         this.num = num;
     }
 }
+
