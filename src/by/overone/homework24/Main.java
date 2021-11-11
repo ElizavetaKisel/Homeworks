c0e64a2ec4098c942dcac8c923356d685303a2fe
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-11 16:16:01 +0300

    create method zodiacSign

--- src/by/overone/homework24/Main.java
+++ src/by/overone/homework24/Main.java
@@ -3,5 +3,8 @@ package by.overone.homework24;
 public class Main {
     public static void main(String[] args) {
 
+        String birthMonth = "march";
+        int birthDay = 30;
+        System.out.println(Month.zodiacSign(birthMonth, birthDay));
     }
 }
--- src/by/overone/homework24/Month.java
+++ src/by/overone/homework24/Month.java
@@ -9,6 +9,85 @@ public enum Month {
         this.days = days;
     }
 
+    public static String zodiacSign(String birthMonth, int birthDay){
+
+        switch (valueOf(birthMonth.toUpperCase())){
+            case MARCH:
+                if(birthDay < 21){
+                    return "Pisces";
+                }else {
+                    return "Aries";
+                }
+            case APRIL:
+                if(birthDay < 21){
+                    return "Pisces";
+                }else {
+                    return "Taurus";
+                }
+            case MAY:
+                if(birthDay < 22){
+                    return "Taurus";
+                }else {
+                    return "Gemini";
+                }
+            case JUNE:
+                if(birthDay < 22){
+                    return "Gemini";
+                }else {
+                    return "Cancer";
+                }
+            case JULY:
+                if(birthDay < 23){
+                    return "Cancer";
+                }else {
+                    return "Leo";
+                }
+            case AUGUST:
+                if(birthDay < 22){
+                    return "Leo";
+                }else {
+                    return "Virgo";
+                }
+            case SEPTEMBER:
+                if(birthDay < 24){
+                    return "Virgo";
+                }else {
+                    return "Libra";
+                }
+            case OCTOBER:
+                if(birthDay < 24){
+                    return "Libra";
+                }else {
+                    return "Scorpio";
+                }
+            case NOVEMBER:
+                if(birthDay < 23){
+                    return "Scorpio";
+                }else {
+                    return "Sagittarius";
+                }
+            case DECEMBER:
+                if(birthDay < 23){
+                    return "Sagittarius";
+                }else {
+                    return "Capricorn";
+                }
+            case JANUARY:
+                if(birthDay < 21){
+                    return "Capricorn";
+                }else {
+                    return "Aquarius";
+                }
+            case FEBRUARY:
+                if(birthDay < 20){
+                    return "Aquarius";
+                }else {
+                    return "Pisces";
+                }
+        }
+        return null;
+    }
+
     public int getDays() {
         return days;
     }
