70ff911a8a48d7aa2cf7f7cd5986b0d7983b0d0f
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-12 23:28:05 +0300

    create Input

--- src/by/overone/homework24/Main.java
+++ src/by/overone/homework24/Main.java
@@ -3,8 +3,9 @@ package by.overone.homework24;
 public class Main {
     public static void main(String[] args) {
 
-        String birthMonth = "march";
-        int birthDay = 30;
-        System.out.println(Month.zodiacSign(birthMonth, birthDay));
+        System.out.println("Enter your birthday: ");
+        Input input = new Input();
+        input.enterBirthday();
+
     }
 }
