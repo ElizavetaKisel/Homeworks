59812655422134a26e629f83060324d5412027cc
ElizavetaKisel <lizavetakisel00@gmail.com>
2021-11-18 14:49:23 +0300

    task2

--- src/by/overone/homework26/Main.java
+++ src/by/overone/homework26/Main.java
@@ -1,5 +1,9 @@
 package by.overone.homework26;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,5 +16,20 @@ public class Main {
         System.out.println("Circle = " + iShapeCircle.getShape(2, Math.PI));
         System.out.println("Square = " +iShapeSquare.getShape(2, 5));
         System.out.println("Triangle = " +iShapeTriangle.getShape(2, 3));
+
+        IComparsion comparsion = (a, b) -> {
+            if (a < b){
+                return a;
+            }else if (a > b){
+                return b;
+            }else {
+                return 0;
+            }
+        };
+        System.out.println("Task 2: ");
+        System.out.println(comparsion.compare(5, 20));
+        System.out.println(comparsion.compare(5, 5));
+
+
     }
 }
